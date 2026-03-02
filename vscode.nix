b99ea2396fc09bc3e0cc218e7cdea421aa2fe2c5
ptdave <ptdave20@gmail.com>
2026-03-02 14:52:25 -0500

    updates

---
+++ vscode.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+    profiles = {
+      default = {
+        extensions = with pkgs.vscode-extensions; [
+          dracula-theme.theme-dracula
+          yzhang.markdown-all-in-one
+        ];
+      };
+    };
+  };
+}
